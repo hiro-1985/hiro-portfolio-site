3dd89b610a59ff4d5c3615b8704d23faada25dec
saitouhirotaka <ten_feet_under@icloud.com>
2022-01-17 00:17:41 +0900

    いろいろ

--- js/main.js
+++ js/main.js
@@ -45,39 +45,30 @@ $(window).on('scroll',function(){
     startPos = winScrollTop;
 });
 
-// テキストアニメーション⬇️
-const textAnimation = document.querySelectorAll(".slider-slide-content")
-for (let i = 0; i < textAnimation.length; i++) {
-  const targetElement = textAnimation[i],
-        texts = targetElement.textContent,
-        textsArray = [];
-
-  targetElement.textContent = "";
-  
-  for (let j = 0; j < texts.split(" ").length; j++) {
-    textsArray.push('<span style= "animation-delay: ' + (j * .1) + 's;">' + texts.split(" ")[j] + '</span>')
-  }
-  for (let k = 0; k < textsArray.length; k++) {
-    targetElement.innerHTML+= textsArray[k];
-  }
-}
-
 
 // スライダー実装（動くWebデザインアイディア帳より参考⬇️）
 var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
 if (windowwidth > 768){
   var responsiveImage = [//PC用の画像
-    { src: './images/firstview-lake-01.jpg'},
-    { src: './images/firstview-tool-02.jpg'},
-    { src: './images/firstview-bass-03.jpg'},
-    { src: './images/firstview-angler-04.jpg'}
+    { src: './images/firstview-lake-01.jpg',
+     text: "<p>「この人を採用して良かった」</p>"},
+    { src: './images/firstview-tool-02.jpg',
+     text: "<p>そう思って頂けるよう、</p>"},
+    { src: './images/firstview-bass-03.jpg',
+     text: "<p>日々、自己研鑽に励み</p>"},
+    { src: './images/firstview-angler-04.jpg',
+     text: "<p>人間性、スキル共に高みを目指します。</p>"}
   ];
 } else {
   var responsiveImage = [//タブレットサイズ（768px）以下用の画像
-    { src: './images/firstview-lake-sp-05.jpg', align: "90%" },
-    { src: './images/firstview-tool-06.jpg' },
-    { src: './images/firstview-bass-sp-07.jpg' },
-    { src: './images/firstview-angler-sp-08.jpg' }
+    { src: './images/firstview-lake-sp-05.jpg', align: "90%",
+      text: "<p>「この人を採用して良かった」</p>"},
+    { src: './images/firstview-tool-06.jpg',
+      text: "<p>そう思って頂けるよう、</p>"},
+    { src: './images/firstview-bass-sp-07.jpg',
+      text: "<p>日々、自己研鑽に励み</p>"},
+    { src: './images/firstview-angler-sp-08.jpg',
+      text: "<p>人間性、スキル共に<br>高みを目指します。</p>"}
   ];
 }
 
@@ -86,11 +77,14 @@ $('#slider').vegas({
   transition: 'blur',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
   transitionDuration: 1500,//切り替わりのアニメーション時間をミリ秒単位で設定
   delay: 10000,//スライド間の遅延をミリ秒単位で。
-  animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
+  animationDuration: 9500,//スライドアニメーション時間をミリ秒単位で設定
   animation: 'kenburnsUp',//スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
   slides: responsiveImage,//画像設定を読む
   //timer:false,// プログレスバーを非表示したい場合はこのコメントアウトを外してください
   cover: true,
+  walk: function (index, slideSettings) {
+    $('.slider-slide-content').html(slideSettings.text);
+}
 });
 
 
@@ -98,10 +92,12 @@ $('#slider').vegas({
 $(function () {
   $(".header__menu__button").click(function() {
     $(".modal, .modal__overlay").fadeIn(200);
+    $(".header__menu__button").fadeOut(200);
   });
 
   $(".header__delete__button, .modal__overlay").click(function() {
     $(".modal, .modal__overlay").fadeOut(200);
+    $(".header__menu__button").fadeIn(200);
   });
 
   $('a[href^="#"]').click(function () {
@@ -170,29 +166,3 @@ $(function() {
     }
   });
 });
-
-// フォームの送信ボタンを押した後の挙動（しょーごさんブログを参考⬇️）
-// $(document).ready(function () {
-
-//   $('#form').submit(function (event) {
-//     var formData = $('#form').serialize();
-//     $.ajax({
-//       url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSe33fiFiL_yw4IP0zh-D4v_LUIi5aMGg66WKfjnb80qb4_FLg/formResponse",
-//       data: formData,
-//       type: "POST",
-//       dataType: "xml",
-//       statusCode: {
-//         0: function () {
-//           $(".end-message").slideDown();
-//           $(".form__submit__outer").fadeOut();
-//           //window.location.href = "thanks.html";
-//         },
-//         200: function () {
-//           $(".false-message").slideDown();
-//         }
-//       }
-//     });
-//     event.preventDefault();
-//   });
-
-// });
